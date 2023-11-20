33ae7c4ed74f955a6d14a42f343fa10258cc895e
Stoycho Kyosev <stoycho.kk@gmail.com>
2023-11-20 21:43:08 +0200

    Add alternative solutions

--- Seminar-08/numeric-systems.cpp
+++ Seminar-08/numeric-systems.cpp
@@ -35,6 +35,7 @@ void displayInvalidDigit(char digit)
 	std::cout << "The passed digit " << digit << " is not well defined." << std::endl;
 }
 
+// Helper functions:
 char getDigit(unsigned digit)
 {
 	if (digit < 10)
@@ -49,6 +50,53 @@ char getDigit(unsigned digit)
 	// '\0' is the char symbol represented as 0.
 	return '\0';
 }
+
+// Returns numeric value for digit represented as char.
+// Digits aren't only the standard digits [0..9] but we use
+// A to represent 10
+// B to represent 11
+// etc
+unsigned getNumber(char ch)
+{
+	if (ch >= '0' && ch <= '9')
+	{
+		return ch - '0';
+	}
+	else if (ch >= 'A' && ch <= 'Z')
+	{
+		return ch - 'A' + 10;
+	}
+	return -1;
+}
+
+// Версиите на решението което не "обръщат" числото в масива
+// т.е. когато имаме 6 в масива за двоична бройна система ще е записано {'1', '1', '0'}
+
+//"from" is in K numeral system. The function converts it to decimal (and the result is stored in an int)
+unsigned int fromRandomToDecimal(const char from[], size_t size, unsigned k)
+{
+	int result = 0;
+
+	for (int i = size - 1, mult = 1; i >= 0; i--, mult *= k) //we declare both the array iter (variable i) and mult in the loop
+		result += getNumber(from[i]) * mult;
+	return result;
+}
+
+
+//converts number from decimal to a number in K numeral system. The result is stored in the array "to"
+void fromDecimalToRandom(unsigned n, char to[], size_t size, unsigned k)
+{
+	for (int arrayIter = size - 1; arrayIter >= 0; arrayIter--) //we fill the array backwards!
+	{
+		int ind = n % k;
+		char current = getDigit(ind);
+		to[arrayIter] = current;
+		n /= k;
+	}
+}
+
+// Версията на функциите които "обръщат" числото в масива.
+// т.е. когато имаме 6 в масив за двоична бройна система ще е записано {'0', '1', '1'}
 
 // returns the number of digits of the number 
 // bufferSize is the size of the array
@@ -72,23 +120,6 @@ int fromDecimal(unsigned number, unsigned base, char result[], unsigned bufferSi
 	return size;
 }
 
-// Returns numeric value for digit represented as char.
-// Digits aren't only the standard digits [0..9] but we use
-// A to represent 10
-// B to represent 11
-// etc
-unsigned getNumber(char ch)
-{
-	if (ch >= '0' && ch <= '9')
-	{
-		return ch - '0';
-	}
-	else if (ch >= 'A' && ch <= 'Z')
-	{
-		return ch - 'A' + 10;
-	}
-	return -1;
-}
 
 // returns the number represented from `number` in base `base`
 // in decimal.
@@ -115,7 +146,7 @@ unsigned toDecimal(char number[], unsigned size, unsigned base)
 }
 
 // Converts number in k-numeric sysytem to number of n-numeric system.
-// returns the number of digits of the new number
+// returns the number of digits of the new number.
 unsigned convertNumericSystems(char numberKNumeric[], unsigned sizeKNumeric, unsigned baseKNumeric, char numberNNumeric[], int sizeNNumeric, unsigned baseNNumeric)
 {
 	unsigned decimalNumber = toDecimal(numberKNumeric, sizeKNumeric, baseKNumeric);
@@ -139,12 +170,10 @@ int main()
 	unsigned number = 12648430;
 
 
-
 	// Convert it to base 16:
 	char hexadecimalNumber[NUMBER_MAX_LEN] = {};
 	unsigned resultLen = fromDecimal(number, 16, hexadecimalNumber, NUMBER_MAX_LEN);
 	showNumber(hexadecimalNumber, resultLen, number, 16);
-
 
 
 	// Convert the base 16 number to base 2:
@@ -153,12 +182,10 @@ int main()
 	showNumber(binaryNumber, binaryNumberLength, number, 2);
 
 
-
 	// Convert the base 2 to base 36:
 	char baseThirtySixNumber[NUMBER_MAX_LEN] = {};
 	unsigned baseThirtySixLen = convertNumericSystems(binaryNumber, binaryNumberLength, 2, baseThirtySixNumber, NUMBER_MAX_LEN, 36);
 	showNumber(baseThirtySixNumber, baseThirtySixLen, number, 36);
-
 
 
 	// Get beck to the original number:
